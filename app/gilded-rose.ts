cf50df833155884c708208c0b96a410914642696
b-dashyan <bagshik@gmail.com>
2025-11-05 23:45:30 +0100

    refactor: lifted up with isSulfuras

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -54,20 +54,17 @@ export class GildedRose {
         if (item.sellIn < 0) {
           item.quality = item.quality - item.quality;
         }
+      } else if (isSulfuras) {
+        // Sulfuras remains unchanged
       } else {
+        // Regular items
         if (item.quality > 0) {
-          if (!isSulfuras) {
-            item.quality = item.quality - 1;
-          }
+          item.quality = item.quality - 1;
         }
-        if (!isSulfuras) {
-          item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
           if (item.quality > 0) {
-            if (!isSulfuras) {
-              item.quality = item.quality - 1;
-            }
+            item.quality = item.quality - 1;
           }
         }
       }
