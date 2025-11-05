0a8d438286bd63410e85f0d4b4a678469a06dfda
b-dashyan <bagshik@gmail.com>
2025-11-05 23:34:38 +0100

    refactor: lifted up with isBackstagePass

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -33,40 +33,40 @@ export class GildedRose {
             item.quality = item.quality + 1;
           }
         }
-      } else {
-        if (!isBackstagePass) {
-          if (item.quality > 0) {
-            if (item.name != "Sulfuras, Hand of Ragnaros") {
-              item.quality = item.quality - 1;
+      } else if (isBackstagePass) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
           }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
+          }
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+          item.quality = item.quality - item.quality;
+        }
+      } else {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1;
           }
         }
         if (item.name != "Sulfuras, Hand of Ragnaros") {
           item.sellIn = item.sellIn - 1;
         }
         if (item.sellIn < 0) {
-          if (!isBackstagePass) {
-            if (item.quality > 0) {
-              if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
-              }
+          if (item.quality > 0) {
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+              item.quality = item.quality - 1;
             }
-          } else {
-            item.quality = item.quality - item.quality;
           }
         }
       }
